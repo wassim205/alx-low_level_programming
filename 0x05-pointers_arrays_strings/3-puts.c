f4ce52426170fa14447fe04cd78987744c948310
wassim205 <wassimelmorabit7@gmail.com>
2023-03-27 09:11:22 -0700

    Tasks

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _puts - prints a string
+ * @str: the string
  *
- * Return: Always 0.
+ * Return: the length of the string
  */
-int main(void)
+void _puts(char *str)
 {
-	char s1[98];
-	char *ptr;
+	int i = 0;
 
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
-	return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
